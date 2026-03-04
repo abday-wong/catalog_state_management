895ce4e9dcbfb2fe0e806485952bb31c9db9b8ff
abday-wong <1123150093@global.ac.id>
2026-03-04 13:49:59 +0700

    membuat tampilan menu catalog

--- lib/features/cart/presentation/pages/catalog_page.dart
+++ lib/features/cart/presentation/pages/catalog_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:week_3_1123150093/core/routes/app_router.dart';
 import 'package:week_3_1123150093/features/cart/domain/entities/product.dart';
+import 'package:week_3_1123150093/features/cart/presentation/widgets/add_button_widget.dart';
 
 class CatalogPage extends StatelessWidget {
   const CatalogPage({super.key});
@@ -13,6 +15,27 @@ class CatalogPage extends StatelessWidget {
       Product(id: '4', name: 'Pangsit Goreng', price: '17000'),
       Product(id: '5', name: 'Bakpao Coklat', price: '12000'),
     ];
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Menu Makanan', style: TextStyle(color: Colors.white,),),
+        centerTitle: true,
+        backgroundColor: Colors.greenAccent,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.shopping_cart, color: Colors.white,),
+            onPressed: () => Navigator.pushNamed(context, AppRouter.cart),
+          ),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: products.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(products[index].name),
+            trailing: AddButtonWidget(product: products[index]),
+          );
+        },
+      ),
+    );
   }
 }
