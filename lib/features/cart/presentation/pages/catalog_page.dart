b364d6c6d4d5ec0a54759241f1bb04aeb6065ab7
abday-wong <1123150093@global.ac.id>
2026-03-04 13:48:31 +0700

    menambahkan menu

--- lib/features/cart/presentation/pages/catalog_page.dart
+++ lib/features/cart/presentation/pages/catalog_page.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:week_3_1123150093/features/cart/domain/entities/product.dart';
 
 class CatalogPage extends StatelessWidget {
   const CatalogPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final products = [
+      Product(id: '1', name: 'Siomay Ayam', price: '15000'),
+      Product(id: '2', name: 'Hakau Udang', price: '20000'),
+      Product(id: '3', name: 'Lumpia Udang', price: '18000'),
+      Product(id: '4', name: 'Pangsit Goreng', price: '17000'),
+      Product(id: '5', name: 'Bakpao Coklat', price: '12000'),
+    ];
     return const Placeholder();
   }
 }
