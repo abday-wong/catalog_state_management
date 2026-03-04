7cd7e39d345af6aeeda2c1cde3a0dd4eac3a7993
abday-wong <1123150093@global.ac.id>
2026-03-04 13:35:19 +0700

    membuat stl add button widget

--- lib/features/cart/presentation/widgets/add_button_widget.dart
+++ lib/features/cart/presentation/widgets/add_button_widget.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
-Widget buildApp(){
-  
+class AddButtonWidget extends StatelessWidget {
+  const AddButtonWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
 }
