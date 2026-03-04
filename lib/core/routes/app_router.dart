15d5e3b6dcc7c4faf00022d14d618bbfa55852ba
abday-wong <1123150093@global.ac.id>
2026-03-04 13:38:18 +0700

    membuat stl CartPage

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,3 +1,11 @@
+import 'package:flutter/material.dart';
+
 class AppRouter {
-  
+  static const String catalog = '/';
+  static const String cart = '/cart';
+
+  static Map<String, WidgetBuilder> get routes => {
+    catalog: (context) => CatalogPage(),
+    cart: (context) => CartPage(),
+  };
 }
