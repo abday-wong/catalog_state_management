ca91fa158d763d8b68c43b80d9ef6590eee8beaa
abday-wong <1123150093@global.ac.id>
2026-03-04 13:40:37 +0700

    membuat kontrak fucntionn di class CartRepository

--- lib/features/cart/domain/repositories/cart_repository.dart
+++ lib/features/cart/domain/repositories/cart_repository.dart
@@ -1,3 +1,8 @@
+import 'package:week_3_1123150093/features/cart/domain/entities/product.dart';
+
 abstract class CartRepository {
-  
+  List<Product> getCartItems();
+  void addItem(Product product);
+  void removeAll();
+  bool isItemInCart(String productId);
 }
